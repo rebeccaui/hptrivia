2c1d10bbb9636044fd3c4d86167317b55b50dae5
rwieberdink <rebeccawieberdink@gmail.com>
2018-06-09 18:15:39 -0400

    linking

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,12 @@
+$(document).ready(function() {
+    console.log("Ready!");
+
+$("#startbutton").click(function() {
+    window.location.href="question1.html";
+});
+
+$("checking").html("Just checking");
+
+});
+
 
