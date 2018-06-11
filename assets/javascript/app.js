29678a5ab279edd2e2738b6369ad8764131b008f
rwieberdink <rebeccawieberdink@gmail.com>
2018-06-11 15:30:39 -0400

    naming files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
+$("#startButton").click(function() {
+    window.location.href="index1.html";
+    
+    });
 
+var timerEnd = 30000;
+
+    setTimeout(function timer() {
+        window.location.href="index4.html";
+
+    }
+})
+    
